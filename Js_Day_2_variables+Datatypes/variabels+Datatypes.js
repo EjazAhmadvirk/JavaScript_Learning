54109f3b64e0a0af667cfa5c75aa62037e1d68f4
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-06 08:02:55 +0500

    datatypes and variables files

---
+++ Js_Day_2_variables+Datatypes/variabels+Datatypes.js
@@ -0,0 +1,31 @@
+let age = 20;
+
+
+let name = "John Doe";
+
+
+let isStudent = true;
+
+
+let height = 5.9; // in feet
+
+
+let subjects = ["Math", "Science", "History"];
+
+
+let address = {
+    street: "123 Main St",
+    city: "Anytown",
+    state: "CA",
+    zip: "12345"
+};
+
+
+console.log("Age:", age);
+console.log("Name:", name);
+console.log("Is Student:", isStudent);
+console.log("Height:", height);
+console.log("Subjects:", subjects);
+console.log("Address:", address);
+console.log("Address City:", address.city);
+console.log("Address State:", address.state);
