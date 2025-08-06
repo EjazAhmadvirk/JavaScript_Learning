6ff805a227a7ea16ccf9ce884101d8af4e002a31
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-06 08:09:44 +0500

    add information about HTML

--- Js_Day_2_variables+Datatypes/variabels+Datatypes.js
+++ Js_Day_2_variables+Datatypes/variabels+Datatypes.js
@@ -35,3 +35,21 @@ console.log("Subjects:", subjects); // Output: ["Math", "Science", "History"]
 console.log("Address:", address);// Output: { street: "123 Main St", city: "Anytown", state: "CA", zip: "12345" }
 console.log("Address City:", address.city);// Output: Anytown
 console.log("Address State:", address.state);// Output: CA
+
+
+// Note: JavaScript is a dynamically typed language, meaning you can change the type of a variable at runtime.
+// For example, we can change the type of 'age' from number to string
+age = "Twenty"; // Now age is a string  
+
+console.log("New Age:", age); // Output: Twenty
+console.log("New Age Type:", typeof age); // Output: string
+
+
+
+
+
+// The output of the above code show on browser console or terminal
+// for output link this file with HTML file and open in browser
+// or run it in a JavaScript environment like Node.js
+// for link with HTML file, you can use the following code:
+// <script src="variabels+Datatypes.js"></script>
