b8887836cd6310a24aac6640bde84e7a85ab9096
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-07 09:38:52 +0500

    for loop file

---
+++ Js_Day_3_input+Loops/for_loop.js
@@ -0,0 +1,6 @@
+for(let i = 0; i < 5; i++) {
+    console.log(`Current value of i: ${i}`);
+}   
+
+// in this loop variable initialized + checked + incremented in one line
+// This code initializes a variable i to 0 and uses a for loop to print the current
