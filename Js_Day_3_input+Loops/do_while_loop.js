de4f1c1495fdac0255172dcb9e86ed9795a1f0db
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-07 09:37:02 +0500

    do while loop file

---
+++ Js_Day_3_input+Loops/do_while_loop.js
@@ -0,0 +1,7 @@
+let i = 0;
+do {
+    console.log(`Current value of i: ${i}`);
+    i++;
+} while (i < 5);
+// This code initializes a variable i to 0 and uses a do-while loop to print the current value of i
+// until i is no longer less than 5. The loop increments i by 1 in each iteration, demonstrating a simple use of a do-while loop in JavaScript.
