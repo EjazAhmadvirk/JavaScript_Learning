24f96a2c2443156b8f8ced65fb46febbac556592
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-08 09:36:57 +0500

    new files here

---
+++ Js_Day_4_if_else+switch/switch.js
@@ -0,0 +1,20 @@
+let garde = 'A';
+switch (garde) {
+    case 'A':
+        console.log("Excellent work!");
+        break;
+    case 'B':
+        console.log("Good job!");
+        break;
+    case 'C':
+        console.log("You passed.");
+        break;
+    case 'D':   
+        console.log("You need to improve.");
+        break;
+    case 'F':
+        console.log("Failed. Better luck next time.");
+        break;
+    default:
+        console.log("Invalid grade.");
+        
