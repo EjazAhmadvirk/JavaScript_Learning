7f183c537b116366b12545e14a66dfafabffdee3
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-11 22:10:53 +0500

    adding in obj_map

--- Js_Day_7_JavaScript_Sturcture/Str_Map.js
+++ Js_Day_7_JavaScript_Sturcture/Str_Map.js
@@ -3,3 +3,31 @@ myMap.set("name", "Ejaz");
 myMap.set("age", 21);
 console.log(myMap.get("name"));   // Ejaz
 console.log(myMap.get("age"));    // 21
+
+
+/*
+These are the properties of the map:
+1. size
+2. set
+3. get
+4. has
+5. delete
+6. clear
+
+
+*/
+myMap.set("name", "Ali");
+console.log(myMap.get("name")); // Ali
+myMap.set("age", 22);
+console.log(myMap.get("age")); // 22
+myMap.set("country", "Pakistan");
+console.log(myMap.get("country")); // Pakistan
+myMap.set("name", "Ali");
+console.log(myMap.get("name")); // Ali
+myMap.set("age", 22);
+console.log(myMap.get("age")); // 22
+
+
+
+
+
