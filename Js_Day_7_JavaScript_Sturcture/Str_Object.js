9c7f244ecd1e6b9691d1ef4b12ed119b48d74e56
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-11 22:07:00 +0500

    str Object file

---
+++ Js_Day_7_JavaScript_Sturcture/Str_Object.js
@@ -0,0 +1,32 @@
+let person = { name: "Ejaz", age: 21 };
+person.country = "Pakistan";     // Add new key
+console.log(person.name);        // Ejaz
+
+
+/*
+These are the properties of the object:
+1. length
+2. push
+3. pop
+4. shift
+5. unshift
+6. splice
+7. slice
+8. concat
+
+*/
+person.name = "Ali"; // Update
+console.log(person.name); // Ali
+person.age = 22; // Update
+console.log(person.age); // 22
+person.country = "Pakistan"; // Add
+console.log(person.country); // Pakistan
+person.name = "Ali"; // Update
+console.log(person.name); // Ali
+person.age = 22; // Update
+
+
+
+
+
+
