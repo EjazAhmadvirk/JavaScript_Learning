658dbae1a5a632f76c76f3ea71742bd62b53493b
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-14 22:14:34 +0500

    new files

---
+++ Js_Day_10_ES6_in_JavaScript/Destructiong_Operators.js
@@ -0,0 +1,9 @@
+// Array destructuring
+let colors = ["red", "green", "blue"];
+let [first, second] = colors;
+console.log(first, second); // red green
+
+// Object destructuring
+let person = { name: "Ejaz", age: 21 };
+let { name: personName, age } = person;
+console.log(personName, age); // Ejaz 21
