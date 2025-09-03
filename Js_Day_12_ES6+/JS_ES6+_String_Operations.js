36d6bd1cfd589a54df5c6b978acafca958d1cb54
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-09-03 08:49:45 +0500

    new stirng file add

---
+++ Js_Day_12_ES6+/JS_ES6+_String_Operations.js
@@ -0,0 +1,16 @@
+const name = "Ejaz";
+const age = 22;
+
+// Old way
+console.log("My name is " + name + " and I am " + age + " years old.");
+
+// ES6 Template Literals
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+// Multi-line string with backticks
+const multiLine = `
+This is line 1
+This is line 2
+This is line 3
+`;
+console.log(multiLine);
