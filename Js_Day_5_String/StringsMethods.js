b2ec64804bc75b382345e9421a8a6831a365c5ca
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-12-15 15:44:04 +0500

    add new files

---
+++ Js_Day_5_String/StringsMethods.js
@@ -0,0 +1,10 @@
+let str1 = 'Hello';
+let str2 = "World";
+let str3 = `Hello World`; // Template literal
+
+
+console.log(str1); //output Hello
+console.log(str2); //output World
+console.log(str3); //output Hello World
+
+
